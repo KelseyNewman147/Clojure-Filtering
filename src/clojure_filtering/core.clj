8ed641a19b97ed5d2a1247fad15a39fd0183df0c
Kelsey <kpnewman2013@gmail.com>
2017-01-31 15:57:43 -0500

    JSONing things

---
+++ src/clojure_filtering/core.clj
@@ -0,0 +1,29 @@
+(ns clojure-filtering.core
+  (:require [clojure.string :as str])
+  (:require [clojure.data.json :as json])
+  (:gen-class))
+
+(defn -main []
+  (println "Choose a category:")
+  (println "Alcohol")
+  (println "Shoes")
+  (println "Furniture")
+  (println "Toiletries")
+  (println "Jewelry")
+  (let [purchases (slurp "purchases.csv")
+        purchases (str/split-lines purchases)
+        purchases (map (fn [line]
+                         (str/split line #","))
+                      purchases)
+                      header (first purchases)
+                      purchases (rest purchases)
+                      purchases (map (fn [line]
+                           (zipmap header line))
+                                     purchases)
+                      category (read-line)
+                      purchases (filter (fn [line]
+                            (= (get line "category") category))
+                             purchases)
+                      file-json (json/write-str purchases)]
+    (spit (str "filtered_purchases_" category ".json")file-json)))
+
